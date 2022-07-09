2a3b2974c1c8918bf7a8e82c4f21f39c746d550e
Raul Correia <raulvinicius245@gmail.com>
2022-07-09 11:53:27 -0300

    Criado rotas: create, list, update, delete e find by Id.

---
+++ src/routes/checklist.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express();
+const Checklist = require('../models/checklist')
+
+//Find All
+router.get('/', async (req, res) => {
+    try {
+        let checkLists = await Checklist.find({});
+        res.status(200).json(checkLists);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+//Find by Id
+router.get('/:id', async (req, res) => {
+    try {
+        let checkList = await Checklist.findById(req.params.id)
+        res.status(200).json(checkList);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+//Create
+router.post('/', async (req, res) => {
+    let { name } = req.body;
+    
+    try {
+        let checkList = await Checklist.create({ name });
+        res.status(201).json(checkList);
+    } catch(error) {
+        res.status(422).json(error);
+    }
+})
+
+//Update
+router.put('/:id', async (req, res) => {
+    let { name } = req.body
+    
+    try {
+        let checkList = await Checklist.findByIdAndUpdate(req.params.id, { "name":name }, { new: true })
+        res.status(200).json(checkList);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+//Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        let checkList = await Checklist.findByIdAndRemove(req.params.id)
+        res.status(200).json(checkList);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+module.exports = router;
